a6ba428a775e5f768af83cd305cb0412776fb83b
prateekm10 <prateekmohan7@gmail.com>
2021-04-03 16:59:13 +0530

    plotcommit

--- plot1.r
+++ plot1.r
@@ -5,3 +5,4 @@ hist(data1$Global_active_power, main="Global Active Power",
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 dev.copy(png, file="plot1.png", height=480, width=480)
 
+dev.off()
